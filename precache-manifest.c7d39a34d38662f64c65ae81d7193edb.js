3d703e3876685c4d3437db6d56b4ac140c370e71
Teague  Ashcraft <tkashcra@usc.edu>
2019-04-26 19:18:12 -0700

    Updates

---
+++ precache-manifest.c7d39a34d38662f64c65ae81d7193edb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "91966b3f0edd57d534a8",
+    "url": "/wonderful/static/js/runtime~main.3434120f.js"
+  },
+  {
+    "revision": "992f2ab07b266a6968ed",
+    "url": "/wonderful/static/js/main.13b114ab.chunk.js"
+  },
+  {
+    "revision": "ba92e18e8d7c9cbf475a",
+    "url": "/wonderful/static/js/2.3d77f90f.chunk.js"
+  },
+  {
+    "revision": "992f2ab07b266a6968ed",
+    "url": "/wonderful/static/css/main.ea348512.chunk.css"
+  },
+  {
+    "revision": "64114413456fdba77244005ce9a7b7df",
+    "url": "/wonderful/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ee9d0e0fc48c3f0f6910f7ee6f8521c6.js"
+  "/wonderful/precache-manifest.c7d39a34d38662f64c65ae81d7193edb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/wonderful/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
